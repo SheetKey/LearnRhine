13600e8844ce6041c371f94434a52c7c1903651d
Will <wveatchw@gmail.com>
2022-07-19 12:59:20 -0400

    newfile

---
+++ game/SDLClock.hs
@@ -0,0 +1,11 @@
+module SDLClock
+  ( SDLClock
+  ) where
+
+import Data.Time.Clock (UTCTime, diffUTCTime)
+
+import qualified SDL
+
+{--------------------------------------------
+Making our event clock
+-}-------------------------------------------
