91458f5bbf3a7e51cbc906eecae83b505a8b61fa
Will <wveatchw@gmail.com>
2022-07-19 14:15:39 -0400

    write clock

--- game/SDLClock.hs
+++ game/SDLClock.hs
@@ -1,11 +1,34 @@
-module SDLClock
-  ( SDLClock
-  ) where
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 
-import Data.Time.Clock (UTCTime, diffUTCTime)
+module SDLClock where
+
+import Data.Time.Clock 
+import Data.Semigroup
+
+import FRP.Rhine
 
 import qualified SDL
 
-{--------------------------------------------
-Making our event clock
--}-------------------------------------------
+
+data SDLClock = SDLClock
+
+instance MonadIO m => Clock m SDLClock where
+  type Time SDLClock = UTCTime
+  type Tag  SDLClock = SDL.Event
+
+  initClock _ = do
+    initialTime <- liftIO getCurrentTime
+    return
+      ( constM $ liftIO $ do
+          event <- SDL.waitEvent
+          time  <- getCurrentTime
+          return (time, event)
+      , initialTime
+      )
+
+instance GetClockProxy SDLClock
+
+instance Semigroup SDLClock where
+  _ <> _ = SDLClock
