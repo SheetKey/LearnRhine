0488b7f64a0cfecaf7dfbed5c60fbf4876006d29
Will <wveatchw@gmail.com>
2022-07-24 15:07:41 -0400

    fix

--- game/SDLInit/SDLInit.hs
+++ game/SDLInit/SDLInit.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module SDLInit.SDLInit where
 
-import Rhine
+import FRP.Rhine
 import qualified SDL
 
-sdlInitAndFlow :: (SDL.Window -> SDL.Renderer -> Rhine IO cl () ()) -> IO ()
+sdlInitAndFlow :: (Clock IO cl, GetClockProxy cl, Time cl ~ Time (In cl), Time cl ~ Time (Out cl))
+               => (SDL.Window -> SDL.Renderer -> Rhine IO cl () ()) -> IO ()
 sdlInitAndFlow rhine = do
   SDL.initializeAll
   window <- SDL.createWindow "Test" SDL.defaultWindow
   renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
   flow $ rhine window renderer
-  
