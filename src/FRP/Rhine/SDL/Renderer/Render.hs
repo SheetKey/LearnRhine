cb646704f25f5a77f45abcfd50450443ba6748ae
Will <wveatchw@gmail.com>
2022-07-25 19:57:51 -0400

    render

---
+++ src/FRP/Rhine/SDL/Renderer/Render.hs
@@ -0,0 +1,21 @@
+module FRP.Rhine.SDL.Renderer.Render
+  ( render
+  )where
+
+import FRP.Rhine
+
+import qualified SDL
+
+-- | A clock that will tick roughly 60 times per second.
+-- Only used for presenting the renderer.
+type FPS60 = Millisecond 17
+
+-- | A 60 fps signal function that presents and clears the renderer's backbuffer.
+-- This is the only function that should be used to present the renderer and should
+-- only be called once in the entire program.
+render :: MonadIO m => SDL.Renderer -> ClSF m FPS60 () ()
+render ren = arrMCl $
+  \_ -> do
+    present ren
+    rendererDrawColor ren $= SDL.V4 0 0 0 0
+    clear ren
