b89a528be7cd45bbb933a4b86bea17628c7b67dd
Will <wveatchw@gmail.com>
2022-07-29 15:19:59 -0400

    remake typeclass

--- src/FRP/Rhine/SDL/Renderer/Renderable.hs
+++ src/FRP/Rhine/SDL/Renderer/Renderable.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE Arrows #-}
+
 module FRP.Rhine.SDL.Renderer.Renderable where
 
 import FRP.Rhine
@@ -10,4 +12,10 @@ import Foreign.C.Types
 -- | A type class for all types that can be rendered to the screen.
 -- It is expected that 'a' has a texture or a means of getting a texture.
 class Renderable a where
-  renderClSF :: MonadIO m => a -> SDL.Renderer -> ClSF m cl Point ()
+  getTexture :: MonadIO m => a -> m SDL.Texture
+  renderClSF :: MonadIO m => a -> SDL.Renderer -> ClSF m cl (Maybe Position) ()
+  renderClSF a ren = arrMCl  
+                     (\mpos -> liftIO $ do
+                         tex <- getTexture a
+                         SDL.copy ren tex Nothing mpos)
+  {-# MINIMAL getTexture #-}
