91277d1b4a6b09aa0dcacf814b864596ac675a5a
Will <wveatchw@gmail.com>
2022-07-30 18:57:12 -0400

    new

---
+++ src/FRP/Rhine/SDL/Renderer/Draw.hs
@@ -0,0 +1,23 @@
+module FRP.Rhine.SDL.Renderer.Draw
+  ( draw
+  ) where
+
+import FRP.Rhine
+
+import FRP.Rhine.SDL.Entity
+
+import qualified SDL
+
+
+drawHelper :: MonadIO m => SDL.Renderer -> [Entity] -> m ()
+drawHelper ren lst = case lst of
+                       []   -> liftIO $ return ()
+                       x:xs -> case getTexture x of
+                                 Just mtex -> liftIO $ do
+                                   tex <- mtex
+                                   SDL.copy ren tex Nothing (getPosition x)
+                                   drawHelper ren xs
+                                 Nothing -> liftIO $ return ()
+
+draw :: MonadIO m => SDL.Renderer -> ClSF m cl [Entity] ()
+draw ren = arrMCl $ drawHelper ren
