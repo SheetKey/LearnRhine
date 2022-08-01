7c29080c207f85ff39731e69ccd8631e36c65636
Will <wveatchw@gmail.com>
2022-08-01 15:53:33 -0400

    draw support for sprites

--- src/FRP/Rhine/SDL/Process/Draw.hs
+++ src/FRP/Rhine/SDL/Process/Draw.hs
@@ -13,12 +13,20 @@ import qualified SDL
 drawHelper :: MonadIO m => SDL.Renderer -> [Entity] -> m ()
 drawHelper ren lst = case lst of
                        []   -> liftIO $ return ()
-                       x:xs -> case getTexture x of
-                                 Just mtex -> liftIO $ do
-                                   tex <- mtex
-                                   SDL.copy ren tex Nothing (fmap mkRectangle $ getPosition x)
-                                   drawHelper ren xs
-                                 Nothing -> liftIO $ return ()
+                       x:xs -> drawSprite ren x
+
+
+drawRect :: MonadIO m => SDL.Renderer -> Entity -> Maybe Rectangle -> m ()
+drawRect ren ent rect = case getTexture ent of
+                              Nothing   -> liftIO $ return ()
+                              Just iotex -> liftIO $ do
+                                tex <- iotex
+                                SDL.copy ren tex rect (fmap mkRectangle $ getPosition ent)
+  
+
+drawSprite :: MonadIO m => SDL.Renderer -> Entity -> m ()
+drawSprite ren ent = drawRect ren ent (fmap spriteRect $ getSprite ent)
+
 
 draw :: MonadIO m => SDL.Renderer -> ClSF m cl [Entity] ()
 draw ren = arrMCl $ drawHelper ren
