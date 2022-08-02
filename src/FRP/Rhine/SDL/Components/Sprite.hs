62aaef579db2c1b5843fb80057d2e5f86ef48318
Will <wveatchw@gmail.com>
2022-08-02 17:39:53 -0400

    fix sprite component

--- src/FRP/Rhine/SDL/Components/Sprite.hs
+++ src/FRP/Rhine/SDL/Components/Sprite.hs
@@ -1,22 +1,26 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds #-}
 
-module FRP.Rhine.SDL.Components.Sprite
-  ( Sprite (..)
-  , spriteRect
-  ) where
+module FRP.Rhine.SDL.Components.Sprite where
 
 import qualified SDL
 import qualified SDL.Image as SDLI
 
 import FRP.Rhine.SDL.Components.Position
 
+import GHC.Generics (Generic)
+import Data.Generics.Product.Fields
+
 data Sprite = Sprite
   { index :: CInt
-  , maxFrames :: CInt
+  , maxFrameIndex :: CInt
   , frameIndex :: CInt
   , width :: Width
   , height :: Height
   }
+  deriving (Generic)
 
 
 spriteRect :: Sprite -> Rectangle
@@ -25,3 +29,12 @@ spriteRect (Sprite {..}) = mkRectangle $ Position (frameIndex * width)
                                                   width
                                                   height
   
+setFrameIndex :: Sprite -> CInt -> Sprite
+setFrameIndex sp i = setField @"frameIndex" i sp
+
+incFrameIndex :: Sprite -> Sprite
+incFrameIndex sp = if ind < max
+                   then setFrameIndex sp (ind + 1)
+                   else setFrameIndex sp 0
+  where max = maxFrameIndex sp
+        ind = frameIndex sp
