cf1e54a907af35e3fb84213d512ad1f45c1e98a8
Will <wveatchw@gmail.com>
2022-08-02 17:40:25 -0400

    fix animation

--- src/FRP/Rhine/SDL/Process/Animate.hs
+++ src/FRP/Rhine/SDL/Process/Animate.hs
@@ -1,12 +1,20 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE Arrows #-}
+
 module FRP.Rhine.SDL.Process.Animate
   ( animate
   ) where
 
 import FRP.Rhine
+import FRP.Rhine.ClSF.Upsample
 
 import qualified SDL
 
+import FRP.Rhine.SDL.Entity
+import FRP.Rhine.SDL.Clock
 import FRP.Rhine.SDL.Components
+
+
 
 
 updateFrame :: Entity -> Entity
@@ -15,5 +23,10 @@ updateFrame ent = setSprite ent (fmap incFrameIndex $ getSprite ent)
 animateHelper :: [Entity] -> [Entity]
 animateHelper = fmap updateFrame
   
-animate :: Monad => BehaviorF m time [Entity] [Entity]
-animate = undefined
+animateFeedback :: Monad m => ClSF m cl ([Entity],CInt) ([Entity],CInt)
+animateFeedback = proc (es, i) -> if mod i 60 == 0
+                                  then returnA -< (animateHelper es, i + 1)
+                                  else returnA -< (es, i + 1)
+
+animate :: Monad m => ClSF m cl [Entity] [Entity]
+animate = feedback 0 animateFeedback
