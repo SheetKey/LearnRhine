2b0e2f76a7fb77e480b2209489d0f22a55dcdab6
Will <wveatchw@gmail.com>
2022-08-04 15:54:35 -0400

    make startFeedback more general

--- src/FRP/Rhine/SDL/Init.hs
+++ src/FRP/Rhine/SDL/Init.hs
@@ -9,6 +9,9 @@ module FRP.Rhine.SDL.Init where
 import FRP.Rhine
 import qualified SDL
 
+import FRP.Rhine.SDL.Entity
+import Data.Void
+
 sdlInitAndFlow :: (Clock IO cl, GetClockProxy cl, Time cl ~ Time (In cl), Time cl ~ Time (Out cl))
                => (SDL.Window -> SDL.Renderer -> Rhine IO cl () ()) -> IO ()
 sdlInitAndFlow rhine = do
@@ -18,8 +21,9 @@ sdlInitAndFlow rhine = do
   flow $ rhine window renderer
 
 
-startFeedback :: Monad m => ClSF m cl ((), a) a
-startFeedback = proc (_, a) -> returnA -< a
+startFeedback :: Monad m => ClSF m cl (b, a) (b, a)
+startFeedback = proc (b, a) -> returnA -< (b, a)
 
 endFeedback :: Monad m => ClSF m cl a ((), a)
 endFeedback = proc a -> returnA -< ((), a)
+
