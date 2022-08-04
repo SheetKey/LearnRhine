f6822c6b633b94cf98e0e5a65c1604cb229d9c83
Will <wveatchw@gmail.com>
2022-08-04 15:54:12 -0400

    add helpers

--- src/FRP/Rhine/SDL/Components/Position.hs
+++ src/FRP/Rhine/SDL/Components/Position.hs
@@ -1,11 +1,15 @@
 module FRP.Rhine.SDL.Components.Position
   ( Position (..)
   , Rectangle
+  , Point
+  , Velocity
   , XPos
   , YPos
   , Width
   , Height
   , mkRectangle
+  , updatePosition
+  , setFromPoint
   , CInt (..)
   ) where
 
@@ -23,7 +27,14 @@ type Height = CInt
 
 data Position = Position XPos YPos Width Height
 
+type Point = (Double, Double)
+type Velocity = (Double, Double)
+
 mkRectangle :: Position -> Rectangle
 mkRectangle (Position x y w l) = SDL.Rectangle (SDL.P (SDL.V2 x y)) (SDL.V2 w l)
 
+updatePosition :: Point -> Position -> Position
+updatePosition (nx, ny) (Position x y w h) = Position (round nx + x) (round ny + y) w h
 
+setFromPoint :: Point -> Position -> Position
+setFromPoint (nx, ny) (Position _ _ w h) = Position (round (100 * nx)) (round (100 * ny)) w h
