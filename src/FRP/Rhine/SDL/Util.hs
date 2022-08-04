9ea9c90ce63d24dd8fe7c880d722b3f2d12730f0
Will <wveatchw@gmail.com>
2022-08-04 15:55:10 -0400

    add utility (delete later same as traceWith (liftIO . print)

--- src/FRP/Rhine/SDL/Util.hs
+++ src/FRP/Rhine/SDL/Util.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE Arrows #-}
+
 module FRP.Rhine.SDL.Util where
 
 import FRP.Rhine
@@ -6,3 +8,8 @@ import qualified SDL
 
 isPressed :: SDL.KeyboardEventData -> Bool
 isPressed kEvent = SDL.keyboardEventKeyMotion kEvent == SDL.Pressed
+
+printInOut :: (MonadIO m, Show a) => ClSF m cl a a
+printInOut = proc a -> do
+  arrMCl (\x -> liftIO $ print x) -< a
+  returnA -< a
