752221532962e7ff0f3dc94c2cd4fdac8c0918a0
Will <wveatchw@gmail.com>
2022-08-05 12:46:44 -0400

    comments and fix name confilct

--- src/FRP/Rhine/SDL/Components/Position.hs
+++ src/FRP/Rhine/SDL/Components/Position.hs
@@ -1,3 +1,13 @@
+{- |
+The 'Position' component of an 'Entity' specifies the
+location and size of the destination rectangle when calling
+'SDL.copy'. The source rectangle is specified in the 'Sprite'
+component.
+
+This module also provides useful functions for working with
+'SDL.Rectangle'.
+-}
+
 module FRP.Rhine.SDL.Components.Position
   ( Position (..)
   , Rectangle
@@ -10,6 +20,7 @@ module FRP.Rhine.SDL.Components.Position
   , mkRectangle
   , updatePosition
   , setFromPoint
+  , mkPoint
   , CInt (..)
   ) where
 
@@ -20,21 +31,35 @@ import Foreign.C.Types
 -- | Represents the points at which an object should be rendered and its size.
 type Rectangle = SDL.Rectangle CInt
 
+-- | The x position is a 'CInt'.
 type XPos = CInt
+-- | The y position is a 'CInt'.
 type YPos = CInt
+-- | The width is a 'CInt'.
 type Width = CInt
+-- | The height is a 'CInt'.
 type Height = CInt
 
+-- | The position component is an x and y position with a width and height.
 data Position = Position XPos YPos Width Height
 
+-- | A point is a tuple of 'Double's. This has an instance in the 'VectorSpace' typeclass.
 type Point = (Double, Double)
+-- | A velocity is a tuple of 'Double's. This has an instance in the 'VectorSpace' typeclass.
 type Velocity = (Double, Double)
 
+-- | A utility for converting. 
 mkRectangle :: Position -> Rectangle
 mkRectangle (Position x y w l) = SDL.Rectangle (SDL.P (SDL.V2 x y)) (SDL.V2 w l)
 
+-- | modifies the current position with a new point.
 updatePosition :: Point -> Position -> Position
 updatePosition (nx, ny) (Position x y w h) = Position (round nx + x) (round ny + y) w h
 
+-- | Sets the new position with a new point.
 setFromPoint :: Point -> Position -> Position
-setFromPoint (nx, ny) (Position _ _ w h) = Position (round (100 * nx)) (round (100 * ny)) w h
+setFromPoint (nx, ny) (Position _ _ w h) = Position (round (nx)) (round (ny)) w h
+
+-- | Converts the position into a point. 
+mkPoint :: Position -> Point
+mkPoint (Position x y _ _) = (fromIntegral x, fromIntegral y)
