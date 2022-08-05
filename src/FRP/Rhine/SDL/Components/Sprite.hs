aaace6680ce0d2698089e2ad72961f60c271b672
Will <wveatchw@gmail.com>
2022-08-05 12:47:00 -0400

    comments

--- src/FRP/Rhine/SDL/Components/Sprite.hs
+++ src/FRP/Rhine/SDL/Components/Sprite.hs
@@ -1,3 +1,10 @@
+{- |
+The 'Sprite' component for an 'Entity' specifies
+whether or not an 'Entity's texture is an animation.
+
+This module also provides functions for working with sprites.
+-}
+
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -13,25 +20,29 @@ import FRP.Rhine.SDL.Components.Position
 import GHC.Generics (Generic)
 import Data.Generics.Product.Fields
 
+-- | The sprite component of an entity.
 data Sprite = Sprite
-  { index :: CInt
-  , maxFrameIndex :: CInt
-  , frameIndex :: CInt
-  , width :: Width
-  , height :: Height
+  { index :: CInt         -- ^ The vertical index.
+  , maxFrameIndex :: CInt -- ^ The max number of frames in the animation. (Starts with 0 not 1.)
+  , frameIndex :: CInt    -- ^ The current horizontal frame index.
+  , width :: Width        -- ^ The pixel width of a frame.
+  , height :: Height      -- ^ The pixel height of a frame.
   }
   deriving (Generic)
 
-
+-- | A function to get the source rectangle from a sprite than should be used
+--   with 'SDL.copy'.
 spriteRect :: Sprite -> Rectangle
 spriteRect (Sprite {..}) = mkRectangle $ Position (frameIndex * width)
                                                   (index * height)
                                                   width
                                                   height
-  
+
+-- | Sets the frame index.
 setFrameIndex :: Sprite -> CInt -> Sprite
 setFrameIndex sp i = setField @"frameIndex" i sp
 
+-- | Increases the frame index by 1. If at max, resets to 0.
 incFrameIndex :: Sprite -> Sprite
 incFrameIndex sp = if ind < max
                    then setFrameIndex sp (ind + 1)
