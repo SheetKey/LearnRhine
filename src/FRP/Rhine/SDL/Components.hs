ba2a687d3b3d35653cacdf309b05766b469c8568
Will <wveatchw@gmail.com>
2022-08-05 12:46:37 -0400

    comments

--- src/FRP/Rhine/SDL/Components.hs
+++ src/FRP/Rhine/SDL/Components.hs
@@ -1,3 +1,8 @@
+{- |
+Each component specifies a property than could belong to any given entity.
+This is based on the Entity Component System (ECS) design principle.
+-}
+
 module FRP.Rhine.SDL.Components
   ( module X
   ) where
