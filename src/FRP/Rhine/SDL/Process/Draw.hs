1f19160934d7109355d19ca7dc5777fd93ac7231
Will <wveatchw@gmail.com>
2022-08-06 13:32:32 -0400

    fix name

--- src/FRP/Rhine/SDL/Process/Draw.hs
+++ src/FRP/Rhine/SDL/Process/Draw.hs
@@ -20,16 +20,16 @@ drawHelper ren lst = sequence $ fmap (drawSprite ren) lst
 
 
 drawRect :: MonadIO m => SDL.Renderer -> Entity -> Maybe Rectangle -> m Entity
-drawRect ren ent rect = case getTexture ent of
+drawRect ren ent rect = case getMTexture ent of
                               Nothing    -> liftIO $ return ent
                               Just iotex -> liftIO $ do
                                 tex <- iotex
-                                SDL.copy ren tex rect (fmap mkRectangle $ getPosition ent)
+                                SDL.copy ren tex rect (fmap mkRectangle $ getMPosition ent)
                                 return ent
   
 
 drawSprite :: MonadIO m => SDL.Renderer -> Entity -> m Entity
-drawSprite ren ent = drawRect ren ent (fmap spriteRect $ getSprite ent)
+drawSprite ren ent = drawRect ren ent (fmap spriteRect $ getMSprite ent)
 
 
 draw :: MonadIO m => SDL.Renderer -> ClSF m cl [Entity] [Entity]
