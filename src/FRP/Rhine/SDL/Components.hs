c3da21c8d9f5cd15ad7c1ef5129a54e571ab2735
Will <wveatchw@gmail.com>
2022-08-06 13:31:01 -0400

    add import

--- src/FRP/Rhine/SDL/Components.hs
+++ src/FRP/Rhine/SDL/Components.hs
@@ -5,7 +5,11 @@ This is based on the Entity Component System (ECS) design principle.
 
 module FRP.Rhine.SDL.Components
   ( module X
+  , CInt (..)
   ) where
+
+import Foreign.C.Types
 
 import FRP.Rhine.SDL.Components.Position as X
 import FRP.Rhine.SDL.Components.Sprite as X
+import FRP.Rhine.SDL.Components.Velocity as X
