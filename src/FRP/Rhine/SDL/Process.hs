f1a25611393b5470c92df8191fcf4cf479bd2379
Will <wveatchw@gmail.com>
2022-08-08 16:36:49 -0400

    add import

--- src/FRP/Rhine/SDL/Process.hs
+++ src/FRP/Rhine/SDL/Process.hs
@@ -8,3 +8,4 @@ import FRP.Rhine.SDL.Process.Active as X
 import FRP.Rhine.SDL.Process.Animate as X
 import FRP.Rhine.SDL.Process.Movement as X
 import FRP.Rhine.SDL.Process.Velocity as X
+import FRP.Rhine.SDL.Process.Collision as X
