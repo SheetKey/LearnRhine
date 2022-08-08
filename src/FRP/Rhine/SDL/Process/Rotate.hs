5008253a8c8255f9a3b7c10554e4b56840109078
Will <wveatchw@gmail.com>
2022-08-08 16:37:29 -0400

    fixes

--- src/FRP/Rhine/SDL/Process/Rotate.hs
+++ src/FRP/Rhine/SDL/Process/Rotate.hs
@@ -1,5 +1,7 @@
 module FRP.Rhine.SDL.Process.Rotate where
 
+import FRP.Rhine
+
 import FRP.Rhine.SDL.Components
 import FRP.Rhine.SDL.Entity
 
@@ -10,20 +12,20 @@ rotateTowards :: (XPos, YPos) -> [Entity] -> [Entity]
 rotateTowards pt@(x, y) [] = []
 rotateTowards pt@(x, y) (e:es) = case getMRotation e of
                                    Nothing  -> e : rotateTowards pt es
-                                   Just rot -> if towardsMouse e
+                                   Just rot -> if towardsMouse rot
                                                then case getMPosition e of
                                                       Nothing -> e : rotateTowards pt es
                                                       Just (Position xP yP _ _) ->
-                                                        let dx = xP - x
-                                                            dy = yP - y
+                                                        let dx = fromIntegral (xP - x)
+                                                            dy = fromIntegral (yP - y)
                                                             angle = (atan2 dy dx) * 180 / 3.14
                                                             newE = setRotation e $
                                                                    Just (setAngle rot angle) 
                                                         in newE : rotateTowards pt es
                                                else e : rotateTowards pt es
 
-rotateTowardsMouse :: [Entity] -> IO [Entity]
+rotateTowardsMouse :: MonadIO m => [Entity] -> m [Entity]
 rotateTowardsMouse ents = do
-  SDL.P (SDL.V x y) <- SDL.getAbsoluteMouseLocation
-  rotateTowards (x, y) ents
+  SDL.P (SDL.V2 x y) <- SDL.getAbsoluteMouseLocation
+  return $ rotateTowards (x, y) ents
 
