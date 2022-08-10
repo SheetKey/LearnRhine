7b4c63732fba95248dcfd2d2c0d09ddd0bc12976
Will <wveatchw@gmail.com>
2022-08-10 13:14:46 -0400

    add to collision

--- src/FRP/Rhine/SDL/Components/Collision.hs
+++ src/FRP/Rhine/SDL/Components/Collision.hs
@@ -13,9 +13,11 @@ import Data.Generics.Product.Fields
 
 
 data Collision a = Collision
-  { hitBox :: (Width, Height)    -- ^ The width and height of the hitbox. 
-  , canHit :: Bool               -- ^ Whether or not an entity can colide. 
-  , hitOther :: a -> a           -- ^ What this entity does to the entity it collides with.
+  { hitBox :: (Width, Height)     -- ^ The width and height of the hitbox. 
+  , canHit :: Bool                -- ^ Whether or not an entity can colide. 
+  , hitOther :: a -> a            -- ^ What this entity does to the entity it collides with.
+  , deactivate :: Bool       -- ^ Whether or not to set self inactive on collision. (Used for entities such as bullets.)
+  , collisionCounter :: Maybe Int -- ^ An entity with collisions might need to track the number of collisions. (Used for bullets that have some amount of peircing.)
   }
   deriving (Generic)
 
@@ -27,3 +29,12 @@ setCanHit c val = setField @"canHit" val c
 
 setHitOther :: Collision a -> (a -> a) -> Collision a
 setHitOther c f = setField @"hitOther" f c
+
+setDeactivate :: Collision a -> Bool -> Collision a
+setDeactivate c f = setField @"deactivate" f c
+
+setCollisionCounter :: Collision a -> Maybe Int -> Collision a
+setCollisionCounter c f = setField @"collisionCounter" f c
+
+incCollisionCounter :: Collision a -> Collision a
+incCollisionCounter c = setCollisionCounter c $ fmap (+ 1) $ collicionCounter c
