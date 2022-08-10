91cc6c390737280917b41706ee2a18f1b5fac0a9
Will <wveatchw@gmail.com>
2022-08-10 14:15:26 -0400

    new files

---
+++ src/FRP/Rhine/SDL/Font.hs
@@ -0,0 +1,26 @@
+module FRP.Rhine.SDL.Font where
+
+import qualified SDL
+import qualified SDL.Font as SDLF
+
+
+loadFont :: MonadIO m
+         -> FilePath
+         -> SDLF.PointSize
+         -> SDLF.Color
+         -> Text
+         -> m SDL.Surface
+loadFont path size color text =
+  bracket (SDLF.load path size) SDLF.free $
+  \f -> SDLF.blended f color text
+
+loadFontTexture :: MonadIO m
+                => SDL.Renderer
+                -> FilePath
+                -> SDLF.PointSize
+                -> SDLF.Color
+                -> Text
+                -> m SDL.Texture
+loadFontTexture r path size color text =
+  bracket (loadFont path size color text) SDL.freeSurface $
+  SDL.createTextureFromSurface r
