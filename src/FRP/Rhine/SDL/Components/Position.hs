85a963be02ae8dc8b0b7acf09d989a96153d25ec
Will <wveatchw@gmail.com>
2022-08-19 13:43:39 -0400

    add cammera support

--- src/FRP/Rhine/SDL/Components/Position.hs
+++ src/FRP/Rhine/SDL/Components/Position.hs
@@ -8,8 +8,14 @@ This module also provides useful functions for working with
 'SDL.Rectangle'.
 -}
 
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module FRP.Rhine.SDL.Components.Position
   ( Position (..)
+  , RenderPosition (..)
   , Rectangle
   , Point
   , XPos
@@ -22,11 +28,17 @@ module FRP.Rhine.SDL.Components.Position
   , mkPoint
   , setWH
   , CInt (..)
+  , setRenderPos
+  , setRenderDest
+  , modifyRelPos
+  , modifyDestPos
   ) where
 
 import qualified SDL
 import Foreign.C.Types
 
+import GHC.Generics (Generic)
+import Data.Generics.Product.Fields
 
 -- | Represents the points at which an object should be rendered and its size.
 type Rectangle = SDL.Rectangle CInt
@@ -43,6 +55,21 @@ type Height = CInt
 -- | The position component is an x and y position with a width and height.
 data Position = Position XPos YPos Width Height
 
+-- | An entity will have a relative position and a destination dictated by the camera.
+data RenderPosition = RenderPosition
+  { getPosition :: Position
+  , getDestination :: Position
+  }
+  deriving (Generic)
+
+-- | Set the relative position.
+setRenderPos :: RenderPosition -> Position -> RenderPosition
+setRenderPos rpos val = setField @"getPosition" val rpos
+
+-- | Set the destination position.
+setRenderDest :: RenderPosition -> Position -> RenderPosition
+setRenderDest rpos val = setField @"getDestination" val rpos
+
 -- | A point is a tuple of 'Double's. This has an instance in the 'VectorSpace' typeclass.
 type Point = (Double, Double)
 
@@ -53,6 +80,14 @@ mkRectangle (Position x y w l) = SDL.Rectangle (SDL.P (SDL.V2 x y)) (SDL.V2 w l)
 -- | Modifies the current position with a new point.
 modifyPosition :: Point -> Position -> Position
 modifyPosition (nx, ny) (Position x y w h) = Position (round nx + x) (round ny + y) w h
+
+-- | Modify the relative position.
+modifyRelPos :: Point -> RenderPosition -> RenderPosition
+modifyRelPos pt rpos = setRenderPos rpos $ modifyPosition pt (getPosition rpos)
+
+-- | Modify the destination position.
+modifyDestPos :: Point -> RenderPosition -> RenderPosition
+modifyDestPos pt rpos = setRenderDest rpos $ modifyPosition pt (getDestination rpos)
 
 -- | Sets the new position with a new point.
 setFromPoint :: Point -> Position -> Position
