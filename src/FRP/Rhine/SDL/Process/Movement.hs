410e4c3580b8b29e341e00aafea983281f6a1b3e
Will <wveatchw@gmail.com>
2022-08-19 13:44:18 -0400

    calculate camera pos

--- src/FRP/Rhine/SDL/Process/Movement.hs
+++ src/FRP/Rhine/SDL/Process/Movement.hs
@@ -40,12 +40,48 @@ import FRP.Rhine.SDL.Components
 --                                ) (0,0) ents
 --  arr setPlayerPos -< (pos ^+^ initPos, ents)
 
-updatePos :: Double -> Entity -> Entity
-updatePos t e = case getMVelocity e of
-                  Nothing  -> e
-                  Just vel -> setPosition e $ fmap (modifyPosition (t *^ vel)) (getMPosition e)
+updateRelPos :: Double -> Entity -> Entity
+updateRelPos t e = case getMVelocity e of
+                     Nothing  -> e
+                     Just vel -> setPosition e $ fmap (modifyRelPos (t *^ vel)) (getMPosition e)
+                     --setPosition e $ fmap (modifyPosition (t *^ vel)) (getMPosition e)
 
-move :: (Monad m, Diff (Time cl) ~ Double) => ClSF m cl [Entity] [Entity]
-move = proc ents -> do
+type Camera = Position
+
+mkCamera :: ([Entity], Camera) -> Camera
+mkCamera ([], c) = c
+mkCamera (e:es, c@(Position cx cy cw ch)) =
+  if isPlayer e
+  then case getMPosition e of
+         Nothing -> c
+         Just (RenderPosition (Position px py _ _) _)
+           -> Position (px - (cw `div` 2)) (py - (ch `div` 2)) cw ch
+  else mkCamera (es, c)
+                      
+cameraBounds :: Camera -> Camera
+cameraBounds (Position x y w h) = Position nx ny w h
+  where nx = if x < 0 then 0 else if x > w then w else x
+        ny = if y < 0 then 0 else if y > h then h else y
+
+updateDest :: Camera -> RenderPosition -> RenderPosition
+updateDest (Position cx cy _ _) rpos@(RenderPosition (Position x y w h) _)
+  = setRenderDest rpos $ Position (x - cx) (y - cy) w h
+
+updateDestPos :: Camera -> Entity -> Entity
+updateDestPos c e = setPosition e $ fmap (updateDest c) (getMPosition e)
+
+updateAllDest :: ([Entity], Camera) -> [Entity]
+updateAllDest (ents, c) = fmap (updateDestPos c) ents
+
+move :: (Monad m, Diff (Time cl) ~ Double) => Camera -> ClSF m cl [Entity] [Entity]
+move c = feedback c $ proc (ents, c) -> do
   _sinceLast <- sinceLastS -< ()
-  returnA -< fmap (updatePos _sinceLast) ents
+
+  nents <- arr id -< fmap (updateRelPos _sinceLast) ents
+
+  nc <- arr mkCamera -< (nents, c)
+  nnc <- arr cameraBounds -< nc
+
+  nnents <- arr updateAllDest -< (nents, nnc)
+
+  returnA -< (nnents, nnc)
