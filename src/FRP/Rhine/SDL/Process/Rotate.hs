6eeb7c30a2540013088bfae3e6a67a9c28e56d3b
Will <wveatchw@gmail.com>
2022-08-19 13:44:26 -0400

    use camera

--- src/FRP/Rhine/SDL/Process/Rotate.hs
+++ src/FRP/Rhine/SDL/Process/Rotate.hs
@@ -10,19 +10,20 @@ import qualified SDL
 
 rotateTowards :: (XPos, YPos) -> [Entity] -> [Entity]
 rotateTowards pt@(x, y) [] = []
-rotateTowards pt@(x, y) (e:es) = case getMRotation e of
-                                   Nothing  -> e : rotateTowards pt es
-                                   Just rot -> if towardsMouse rot
-                                               then case getMPosition e of
-                                                      Nothing -> e : rotateTowards pt es
-                                                      Just (Position xP yP w h) ->
-                                                        let dx = fromIntegral (xP - x) + ((fromIntegral w) / 2)
-                                                            dy = fromIntegral (yP - y) + ((fromIntegral h) / 2)
-                                                            angle = ((atan2 dy dx) * 180 / 3.14) - 90
-                                                            newE = setRotation e $
-                                                                   Just (setAngle rot angle) 
-                                                        in newE : rotateTowards pt es
-                                               else e : rotateTowards pt es
+rotateTowards pt@(x, y) (e:es) =
+  case getMRotation e of
+    Nothing  -> e : rotateTowards pt es
+    Just rot -> if towardsMouse rot
+                then case getDestination <$> getMPosition e of
+                       Nothing -> e : rotateTowards pt es
+                       Just (Position xP yP w h) ->
+                         let dx = fromIntegral (xP - x) + ((fromIntegral w) / 2)
+                             dy = fromIntegral (yP - y) + ((fromIntegral h) / 2)
+                             angle = ((atan2 dy dx) * 180 / 3.14) - 90
+                             newE = setRotation e $
+                                    Just (setAngle rot angle) 
+                         in newE : rotateTowards pt es
+                else e : rotateTowards pt es
 
 rotateTowardsMouse :: MonadIO m => [Entity] -> m [Entity]
 rotateTowardsMouse ents = do
