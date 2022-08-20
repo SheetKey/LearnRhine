aa44c4bbeff309a544c156922c10f1f7ae206c77
Will <wveatchw@gmail.com>
2022-08-20 17:16:13 -0400

    undo comments

--- src/FRP/Rhine/SDL/Process/Draw.hs
+++ src/FRP/Rhine/SDL/Process/Draw.hs
@@ -12,26 +12,6 @@ import FRP.Rhine.SDL.Components
 import qualified SDL
 
 
---type Camera = Position
---
---
---mkCamera :: ([Entity], Camera) -> Camera
---mkCamera ([], c) = c
---mkCamera (e:es, c@(Position cx cy cw ch)) =
---  if isPlayer e
---  then case getMPosition e of
---         Nothing -> c
---         Just (Position px py _ _) -> Position (px - (cw `div` 2)) (py - (ch `div` 2)) cw ch
---  else mkCamera (es, c)
---                      
---cameraBounds :: Camera -> Camera
---cameraBounds (Position x y w h) = Position nx ny w h
---  where nx = if x < 0 then 0 else if x > w then w else x
---        ny = if y < 0 then 0 else if y > h then h else y
---
---updateDest :: Camera -> Position -> Position
---updateDest (Position cx cy _ _) (Position x y w h) = Position (x - cx) (y - cy) w h
-               
 
 drawRect :: MonadIO m => SDL.Renderer -> Entity -> Maybe Rectangle -> m Entity
 drawRect ren ent rect =
