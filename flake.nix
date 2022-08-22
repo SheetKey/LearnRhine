55ecbdead86a46dcfa786e466aed49550633371d
Will <wveatchw@gmail.com>
2022-08-21 21:02:38 -0400

    unstable rhine to use Feedback SN constructor

--- flake.nix
+++ flake.nix
@@ -4,11 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+
+    rhine-unstable = {
+      url = "github:turion/rhine";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
+  outputs = { self, nixpkgs, flake-utils, rhine-unstable }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
+
         pkgs = nixpkgs.legacyPackages.${system};
 
         haskellPackages = pkgs.haskellPackages;
@@ -24,9 +30,11 @@
             # Dependency overrides go here
             # Ex with gi-gtk-declarative:
             # If version is broken then:
-            # gi-gtk-declarative = jailbreakUnbreak haskeppPackages.gi-gtk-declarative;
+            # gi-gtk-declarative = jailbreakUnbreak haskellPackages.gi-gtk-declarative;
             # or if tests failing: 
             # gi-gtk-declarative = pkgs.haskell.lib.dontCheck haskellPackages.gi-gtk-declarative;
+
+            rhine = haskellPackages.callCabal2nix "rhine" (rhine-unstable + "/rhine") { };
           };
 
         defaultPackage = self.packages.${system}.${packageName};
@@ -37,26 +45,27 @@
             cabal-install
             haskell-language-server
             haskellPackages.implicit-hie
+            cabal2nix
 
-            # Rhine
-            haskellPackages.rhine
-            haskellPackages.vector-sized
+            ## Rhine
+            #haskellPackages.rhine
+            #haskellPackages.vector-sized
 
-            # SLD2
-            haskellPackages.sdl2
-            haskellPackages.sdl2-image
-            haskellPackages.sdl2-ttf
-            SDL2
-            pkg-config
+            ## SLD2
+            #haskellPackages.sdl2
+            #haskellPackages.sdl2-image
+            #haskellPackages.sdl2-ttf
+            #SDL2
+            #pkg-config
 
-            # Random
-            haskellPackages.random
+            ## Random
+            #haskellPackages.random
 
-            # Generic lens
-            haskellPackages.generic-lens
+            ## Generic lens
+            #haskellPackages.generic-lens
 
-            # STM
-            haskellPackages.stm_2_5_1_0
+            ## STM
+            #haskellPackages.stm_2_5_1_0
           ];
           inputsFrom = builtins.attrValues self.packages.${system};
         };
