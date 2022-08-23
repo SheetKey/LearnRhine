2d469d027cad0be3d9f592590c4f0dcc570f242b
Will <wveatchw@gmail.com>
2022-08-23 14:50:14 -0400

    new loop using feedback

--- src/FRP/Rhine/SDL/Init.hs
+++ src/FRP/Rhine/SDL/Init.hs
@@ -38,12 +38,25 @@ sdlInitAndFlow rhine = do
   flow $ rhine window renderer
 
 
-startFeedback :: Monad m => ClSF m cl (b, a) a
-startFeedback = proc (b, a) -> returnA -< a
+startFeedback :: Monad m => ClSF m cl ((), a) a
+startFeedback = proc (_, a) -> returnA -< a
 
+-- | Depreciate
 startFeedbackWith :: Monad m => ClSF m cl (b, a) (b, a)
 startFeedbackWith = proc (b, a) -> returnA -< (b, a)
 
 endFeedback :: Monad m => ClSF m cl a ((), a)
 endFeedback = proc a -> returnA -< ((), a)
 
+
+sdlInitAndFlowLoop :: (Clock IO cl, Clock IO (In cl), Clock IO (Out cl), GetClockProxy cl, Time cl ~ Time (In cl), Time cl ~ Time (Out cl))
+                   => gameState
+                   -> (SDL.Window -> SDL.Renderer -> Rhine IO cl gameState gameState)
+                   -> IO ()
+sdlInitAndFlowLoop initial rhine = do
+  SDL.initializeAll
+  SDLI.initialize [ SDLI.InitPNG ]
+  SDLF.initialize
+  window <- SDL.createWindow "Test" myDefaultWindow
+  renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
+  flow $ feedbackRhine (keepLast initial) $ startFeedback ^->@ (rhine window renderer) @>-^ endFeedback
