4544f8c7b7225328054a29726ee777dcb10c7417
Will <wveatchw@gmail.com>
2022-09-22 19:07:37 -0400

    fixes

--- src/FRP/Rhine/Extra/Schedule.hs
+++ src/FRP/Rhine/Extra/Schedule.hs
@@ -1,19 +1,20 @@
 module FRP.Rhine.Extra.Schedule where
 
 import FRP.Rhine
+import Data.MonadicStreamFunction.Async (concatS)
 
-
-duplicateDualTick :: MSF m () (time, Either (Either a b) c) -> MSF m () (time, Either (Either a b) (Either a c))
+duplicateDualTick :: Monad m => MSF m () (time, Either (Either a b) c) -> MSF m () (time, Either (Either a b) (Either a c))
 duplicateDualTick runningClock = concatS $ runningClock >>> arr dupA
   where
     dupA (time, Left (Left  a)) = [ (time, Left (Left a)), (time, Right (Left a)) ]
     dupA (time, Left (Right b)) = [ (time, Left (Right b)) ]
-    dupA (time, Right c         = [ (time, Right (Right c)) ]
+    dupA (time, Right c)        = [ (time, Right (Right c)) ]
 
-schedDualPar :: Monad m => Schedule m (ParClock m cla clb) clc -> Schedule m (ParClock m cla clb) (ParClock m cla clc)
-schedDualPar sched = Schedule $ \ parab parac ->
+schedDualPar :: Monad m
+             => Schedule m (ParClock m cla clb) clc
+             -> Schedule m (ParClock m cla clb) (ParClock m cla clc)
+schedDualPar sched = Schedule $ \parab parac -> do
   let clc = parallelCl2 parac
-      --newClock = ParallelClock parab clc sched
-  in do
-    (runningClock, initTime) <- (initSchedule sched) parab clc
-    return (duplicateDualTick runningClock, initTime
+  (runningClock, initTime) <- (initSchedule sched) parab clc
+  return (duplicateDualTick runningClock, initTime)
+
